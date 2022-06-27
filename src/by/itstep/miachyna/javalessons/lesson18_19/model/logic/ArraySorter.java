2bcdeb9650a5d09abbbf83a6674d145244acc0df
Miachyna-Alena <vysotskayaalena5@gmail.com>
2022-06-27 13:39:41 +0300

    Add bubble and selected sorting algorithms

---
+++ src/by/itstep/miachyna/javalessons/lesson18_19/model/logic/ArraySorter.java
@@ -0,0 +1,36 @@
+package by.itstep.miachyna.javalessons.lesson18_19.model.logic;
+
+public class ArraySorter {
+    public static void bubbleSortAcs(int[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            boolean flag = true;
+
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                if (array[i] < array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    flag = false;
+                }
+            }
+            if(flag){
+                break;
+            }
+        }
+    }
+
+    public static void selectedSortAcs(int[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            int index = j;
+            for (int i = index + 1; i < array.length; i++) {
+                if (array[i] > array[index]) {
+                    index = i;
+                }
+            }
+
+            int temp = array[j];
+            array[j] = array[index];
+            array[index] = temp;
+        }
+    }
+}
